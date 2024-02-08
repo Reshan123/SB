3dd95c58acc4e21feceb022cee6a0e59417e255a
Reshan Gomis <reshangomis@gmail.com>
2024-02-08 07:11:50 +0530

    Added Navbar and Header of homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,19 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Home from './views/home/home';
+import Navbar from './views/navbar/navbar';
 
 function App() {
   return (
-    <></>
+    <Router>
+      
+      <Navbar home = "/" aboutus = "/" communityDetails = "/" events = "/" contactUs = "/" gallery = "/" login = "/" news="/" upcomingEvents="/" pastEvents="/"  />
+      
+      <Routes>
+        <Route path="/" element={<Home />} />
+      
+      </Routes>
+    </Router>
   );
 }
 
