cc7905e4c7ced2509218309df6d1d550721de730
DeanneJ <139200076+DeanneJ@users.noreply.github.com>
2024-04-17 20:41:01 +0530

    frontend updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,17 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './views/home/home';
 import Navbar from './views/navbar/navbar';
+import Contactus from './views/contactus/contactus';
 
 function App() {
   return (
     <Router>
       
-      <Navbar home = "/" aboutus = "/" communityDetails = "/" events = "/" contactUs = "/" gallery = "/" login = "/" news="/" upcomingEvents="/" pastEvents="/"  />
+      <Navbar home = "/" aboutus = "/" communityDetails = "/" events = "/" contactUs = "/contact-us" gallery = "/" login = "/" news="/" upcomingEvents="/" pastEvents="/"  />
       
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/contact-us" element={<Contactus />} />
       
       </Routes>
     </Router>
