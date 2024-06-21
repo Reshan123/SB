edcc4142ccd9ebc894e7115571e8287ac022da60
Reshan Gomis <reshangomis@gmail.com>
2024-06-21 12:16:40 +0530

    scroll to top added to navbar

--- frontend/src/views/navbar/navbar.js
+++ frontend/src/views/navbar/navbar.js
@@ -5,7 +5,9 @@ import IEEE from '../../images/logos/IEEE.png'
 
 const Navbar = ({home , aboutus , communityDetails , events , contactUs , gallery , login, news, upcomingEvents, pastEvents}) => {
     
-    
+    const scrollToTop = () => {
+        window.scroll(0, 0);
+    }
 
     const buttonClick = () => {
         const navBarID = document.getElementById("navBar")
@@ -16,12 +18,12 @@ const Navbar = ({home , aboutus , communityDetails , events , contactUs , galler
         <nav>
             <div className="navLogo"><img src={IEEE} alt="" /></div>
             <div className="navLinksContainer">
-                <div className="navLink"><NavLink to={ home }>Home</NavLink></div>
-                <div className="navLink"><NavLink to={ aboutus }>About Us</NavLink></div>
-                <div className="navLink"><NavLink to={ contactUs }>Contact Us</NavLink></div>
-                <div className="navLink"><NavLink to={ communityDetails }>Community Details</NavLink></div>
-                {/* <div className="navLink"><NavLink to={ gallery }>Gallery</NavLink></div> */}
-                <div className="navLink"><NavLink to={ login }>Log In / Sign Up</NavLink></div>
+                <div className="navLink"><NavLink onClick={() => {scrollToTop()}} to={ home }>Home</NavLink></div>
+                <div className="navLink"><NavLink onClick={() => {scrollToTop()}} to={ aboutus }>About Us</NavLink></div>
+                <div className="navLink"><NavLink onClick={() => {scrollToTop()}} to={ contactUs }>Contact Us</NavLink></div>
+                <div className="navLink"><NavLink onClick={() => {scrollToTop()}} to={ communityDetails }>Community Details</NavLink></div>
+                {/* <div className="navLink"><NavLink onClick={() => {scrollToTop()}} to={ gallery }>Gallery</NavLink></div> */}
+                <div className="navLink"><NavLink onClick={() => {scrollToTop()}} to={ login }>Log In / Sign Up</NavLink></div>
             </div>
             <div className="navCollapsedNav" id='navBar'>
                 <div className="navCollapsedNavButton" onClick={buttonClick}>
@@ -30,11 +32,11 @@ const Navbar = ({home , aboutus , communityDetails , events , contactUs , galler
                     <div className="navBarCollpasedNavButtonDiv"></div>
                 </div>
                 <div className="navCollapsedNavLinks" >
-                    <div className="navCollapsedNavLink"><NavLink to={ communityDetails }>Community Details</NavLink></div>
-                    <div className="navCollapsedNavLink"><NavLink to={ events }>Events</NavLink></div>
-                    <div className="navCollapsedNavLink"><NavLink to={ upcomingEvents }>Upcoming Events</NavLink></div>
-                    <div className="navCollapsedNavLink"><NavLink to={ pastEvents }>Past Events</NavLink></div>
-                    <div className="navCollapsedNavLink"><NavLink to={ news }>News</NavLink></div>
+                    <div className="navCollapsedNavLink"><NavLink onClick={() => scrollToTop()} to={ communityDetails }>Community Details</NavLink></div>
+                    <div className="navCollapsedNavLink"><NavLink onClick={() => scrollToTop()} to={ events }>Events</NavLink></div>
+                    <div className="navCollapsedNavLink"><NavLink onClick={() => scrollToTop()} to={ upcomingEvents }>Upcoming Events</NavLink></div>
+                    <div className="navCollapsedNavLink"><NavLink onClick={() => scrollToTop()} to={ pastEvents }>Past Events</NavLink></div>
+                    <div className="navCollapsedNavLink"><NavLink onClick={() => scrollToTop()} to={ news }>News</NavLink></div>
                 </div>
             </div>
         </nav>
